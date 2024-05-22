7da4305ec74a8384de2c961564c4169362e8fb6b
Petr Balat <petr.balat@gmail.com>
2024-05-22 20:00:14 +0200

    spring state machine dependency
    - https://docs.spring.io/spring-statemachine/docs/3.2.1/reference/#using-gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.statemachine:spring-statemachine-starter:3.2.1")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.flywaydb:flyway-core")
